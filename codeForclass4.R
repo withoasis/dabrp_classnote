83d2866f3b867775cb6cf849209f6dfb8c9db4dc
mrchypark <mrchypark@gmail.com>
2017-04-27 19:24:55 +0900

    class

--- codeForclass4.R
+++ codeForclass4.R
@@ -5,8 +5,22 @@ library(RMySQL)
 
 # tem<-fread("./bank/train_ver2.csv")
 con <- dbConnect(dbDriver("MySQL"), dbname = "bank", user = "root", 
-                 password = "XXXXXX")
+                 password = "XXXXX")
 
-query.result <- dbSendQuery(con, "SELECT * FROM train limit 10")
-test.table <- fetch(query.result)
-test.table
+dbListTables(con)
+dbWriteTable(con, "mtcars", mtcars)
+dbListTables(con)
+
+dbListFields(con, "train")
+dbReadTable(con, "mtcars")
+
+query <- dbSendQuery(con, "select * from train limit 10")
+train10 <- dbFetch(query)
+train10
+
+dbClearResult(query)
+
+dbDisconnect(con)
+
+
+
